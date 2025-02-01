512ff2d7b8e1507bc3a55c5fcbafbf509016580e
Poncholo Riego de Dios <poncholoriegodedios@gmail.com>
2025-02-01 16:57:25 +0800

    Pasting codes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,67 @@
+<script lang="ts">
+	import CurrencyList from './CurrencyList.svelte';
+	import Header from './Header.svelte';
+	import Output from './Output.svelte';
+
+	let apiKey = 'cfa119cbaabf69fb4a42fdc1';
+	let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+	let {
+		name
+	}: {
+		name: string;
+	} = $props();
+
+	let input = $state({
+		value: ``, //valueInput
+		from: ``, //fromConvert
+		to: `` //toConvert
+	});
+
+	let show = $state({
+		input: '', //showInput
+		from: ``, //showFrom
+		to: `` //showTo
+	});
+
+	let display = $state({
+		exchangeOut: 0,
+		outputStatus: false
+	});
+
+	//	let valueInput;
+	//	let showInput;
+	//	let fromConvert = ``;
+	//	let toConvert = ``;
+	//	let showFrom = ``;
+	//	let showTo = ``;
+	//	let exchangeOut = 0;
+	//	let outputStatus = false;
+
+	async function Convert() {
+		if (isFinite(input.value) && input.value > 0 && input.from && input.to) {
+			try {
+				const response = await fetch(`${api}${input.from}`);
+				const data = await response.json();
+				if (data.conversion_rates) {
+					let fromExchange = data.conversion_rates[input.from];
+					let toExchange = data.conversion_rates[input.to];
+					display.exchangeOut = (input.value / fromExchange) * toExchange;
+					display.outputStatus = true;
+					show.from = input.from;
+					show.to = input.to;
+					show.input = input.value;
+				}
+			} catch (error) {
+				alert('Error fetching exchange rates:', error);
+			}
+		} else {
+			alert('Invalid input or currency selection.');
+		}
+	}
+</script>
+
+//+page.svelte
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
